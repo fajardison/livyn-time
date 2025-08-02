27cbb1e61ac5c83bc0fc7402a543a1ed267b9ced
Dimas Fajar <153111942+fajardison@users.noreply.github.com>
2025-08-03 01:44:31 +0700

    Create timezoneMap.js

---
+++ constants/timezoneMap.js
@@ -0,0 +1,24 @@
+const timezoneMap = {
+  'Asia/Jakarta':         { abbr: 'WIB',  country: 'Indonesia' },
+  'Asia/Makassar':        { abbr: 'WITA', country: 'Indonesia' },
+  'Asia/Jayapura':        { abbr: 'WIT',  country: 'Indonesia' },
+  'Asia/Tokyo':           { abbr: 'JST',  country: 'Japan' },
+  'Asia/Seoul':           { abbr: 'KST',  country: 'South Korea' },
+  'Asia/Shanghai':        { abbr: 'CST',  country: 'China' },
+  'Asia/Kolkata':         { abbr: 'IST',  country: 'India' },
+  'Asia/Dubai':           { abbr: 'GST',  country: 'United Arab Emirates' },
+  'Europe/London':        { abbr: 'GMT',  country: 'United Kingdom' },
+  'Europe/Berlin':        { abbr: 'CET',  country: 'Germany' },
+  'Europe/Moscow':        { abbr: 'MSK',  country: 'Russia' },
+  'America/New_York':     { abbr: 'EST',  country: 'United States' },
+  'America/Chicago':      { abbr: 'CST',  country: 'United States' },
+  'America/Denver':       { abbr: 'MST',  country: 'United States' },
+  'America/Los_Angeles':  { abbr: 'PST',  country: 'United States' },
+  'America/Sao_Paulo':    { abbr: 'BRT',  country: 'Brazil' },
+  'Africa/Cairo':         { abbr: 'EET',  country: 'Egypt' },
+  'Africa/Nairobi':       { abbr: 'EAT',  country: 'Kenya' },
+  'Australia/Sydney':     { abbr: 'AEST', country: 'Australia' },
+  'Pacific/Auckland':     { abbr: 'NZST', country: 'New Zealand' },
+};
+
+export default timezoneMap;
